e670b1b9c27846a892f3716968fd919bc18e8a0e
Trevor J Hoglund <trewbot@phene.co>
2016-03-31 16:05:07 -0500

    Updated Test

--- src/cs235/quiz4/Test/Test.java
+++ src/cs235/quiz4/Test/Test.java
@@ -1,29 +1,19 @@
 package cs235.quiz4.Test;
-
 import cs235.quiz4.Interface.Movable;
 import cs235.quiz4.MovableShapes.*;
-
 public class Test {
-
 	public static void main(String[] args) {
-		
-		//MovablePoint is a sub-type of Movable
-		Movable m1 = new MovablePoint(5, 6, 10, 15);     
-		System.out.println(m1);
-		m1.moveLeft();
-		System.out.println(m1);
-		   
-		//MovableCircle is a sub-type of Movable
-		Movable m2 = new MovableCircle(2, 1, 2, 20, 1); 
-		System.out.println(m2);
-		m2.moveRight();
-		System.out.println(m2);
-		
-		//	MovableRectangle is a sub-type of Movable
-		Movable m3 = new MovableRectangle(1,1,4,5,3,3);
-		System.out.println(m3);
-		m3.moveDown();
-		System.out.println(m3);
+		Movable[] shapes = new Movable[6];
+		for(int i = 0; i < shapes.length; i++)
+			shapes[i] = i % 2 > 0 ? new MovableCircle(i,i,1,1,i) : new MovableRectangle(i,i,2*i,2*i,1,1);
+		for(Movable i : shapes){
+			System.out.println(i);
+			i.moveUp();
+			i.moveDown();
+			i.moveRight();
+			i.moveLeft();
+			System.out.println(i);
+			System.out.println();
+		}
 	}
-
 }

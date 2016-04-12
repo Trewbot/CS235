a2f38482540dfe300d04449ecc3186ae2ac5fa2b
Trevor J Hoglund <trewbot@phene.co>
2016-04-12 15:49:03 -0500

    Condensed more

--- src/cs235/quiz5/TestArrayList.java
+++ src/cs235/quiz5/TestArrayList.java
@@ -1,19 +1,13 @@
 package cs235.quiz5;
 import java.util.*;
 public class TestArrayList {
-	public static void switchPairs(List<String> words){
+	public static List<String> switchPairs(List<String> words){
 		for(int i = 0; i < words.size() - 1; i += 2) words.set(i, words.set(i + 1, words.get(i)));
+		return words;
 	}
 	public static void main(String[] args){
 		List<String>	even = new ArrayList<>(Arrays.asList("four","score","and","seven","years","ago")),
 						odd = new ArrayList<>(Arrays.asList("to","be","or","not","to","be","hamlet"));
-		System.out.println("BEFORE:");
-		System.out.println(even);
-		System.out.println(odd);
-		System.out.println("\nAFTER:");
-		switchPairs(even);
-		System.out.println(even);
-		switchPairs(odd);
-		System.out.println(odd);
+		System.out.println("BEFORE:\n" + even + "\n" + odd + "\n\nAFTER:\n" + switchPairs(even) + "\n" + switchPairs(odd));
 	}
 }
